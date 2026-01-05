638ee1e4acde7b77f76ceb90d82ff44f2019f009
Vágó Benedek <benedek.vago@HUL-0140.local>
2026-01-05 12:08:01 +0100

    Initial commit

---
+++ YettelHomeAssignment/YettelHomeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  YettelHomeAssignmentApp.swift
+//  YettelHomeAssignment
+//
+//  Created by Vágó Benedek on 2026. 01. 05..
+//
+
+import SwiftUI
+
+@main
+struct YettelHomeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
