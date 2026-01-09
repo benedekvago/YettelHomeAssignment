08d23037c87808eb0d05e905eb092f4c74f66179
Vágó Benedek <benedek.vago@HUL-0140.local>
2026-01-09 15:40:03 +0100

    Remove git files from accidental inner repository

---
+++ ApiClient/Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ApiClient",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v10_15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ApiClient",
+            targets: ["ApiClient"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-openapi-generator",
+            from: "1.0.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-openapi-runtime",
+            from: "1.0.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-openapi-urlsession",
+            from: "1.0.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "ApiClient",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession")
+            ],
+            publicHeadersPath: ".",
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
+            ]
+        )
+    ]
+)
